f26b31504e91383fb59c9aaaf89fdd24586f2e5c
Raphael Abenom <86538018+raphaelabenom@users.noreply.github.com>
2023-02-04 20:46:22 -0300

    Update pytubescriptvideodownload.py

--- pytubescriptvideodownload.py
+++ pytubescriptvideodownload.py
@@ -2,6 +2,6 @@
 from pytube import YouTube
 #Entry your video link
 video = YouTube("Video link")
-streams = video.streams.filter(progressive=True).order_by("resolution").last()
+streams = video.streams.filter(progressive=True, file_extension='mp4').order_by("resolution").last()
 streams.download()
 print("Video Downloaded")
